6c931252e09c990b0bab0329aec475d2f176ce28
min23asdw <min23asdw@outlook.com>
2021-02-02 16:07:39 +0700

    Sus

--- lab14_4.cpp
+++ lab14_4.cpp
@@ -7,13 +7,27 @@ void shuffle(int &,int &,int &,int &);
 
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
-	
-	srand(time(0));	
-	
+
+	srand(time(0));
+
 	for(int i = 0;i < 10;i++){
 	    shuffle(a,b,c,d);
 	    cout << a << " " << b << " " << c << " " << d << "\n";
 	}
-	
+
 	return 0;
+}
+
+void shuffle(int &a,int &b,int &c,int &d){
+	int money[] = {50,100,500,1000};
+	for(int i = 0; i < 4 ; i++){
+		int rd = rand()%4;
+		int moneypre = money[i];
+		money[i] = money[rd];
+		money[rd] = moneypre;
+	}
+	a = money[0];
+	b = money[1];
+	c = money[2];
+	d = money[3];
 }
